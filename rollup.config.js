6964b1ef970c80de92f47ef30152277618f35bda
Filip Tomeš <filiptomes@protonmail.com>
2022-02-10 22:34:08 +0100

    boilerplate integration

---
+++ rollup.config.js
@@ -0,0 +1,102 @@
+import svelte from 'rollup-plugin-svelte';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
+import css from 'rollup-plugin-css-only';
+import { sveltePreprocess } from 'svelte-preprocess/dist/autoProcess';
+
+const production = !process.env.ROLLUP_WATCH;
+// add more items into pages array to be include those pages
+// for multi page build
+const pages = ['home'];
+
+function serve() {
+	let server;
+
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+	return {
+		writeBundle() {
+			if (server) return;
+			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+				stdio: ['ignore', 'inherit', 'inherit'],
+				shell: true
+			});
+
+			process.on('SIGTERM', toExit);
+			process.on('exit', toExit);
+		}
+	};
+}
+
+const multiSPA = page => {
+	if (page === 'home') {
+		return {
+			input: 'src/pages/home/home.js',
+			output: {
+				sourcemap: true,
+				format: 'iife',
+				name: 'app',
+				file: 'public/build/home.js'
+			},
+			plugins: [
+				svelte({
+					preprocess: sveltePreprocess(),
+					compilerOptions: {
+						dev: !production
+					}
+				}),
+				css({ output: 'home.css' }),
+				resolve({
+					browser: true,
+					dedupe: ['svelte']
+				}),
+				commonjs(),
+				!production && serve(),
+				!production && livereload('public'),
+				production && terser()
+			],
+			watch: {
+				clearScreen: false
+			}
+		}
+	}
+
+	return {
+		input: `src/pages/${page}/${page}.js`,
+		output: {
+			sourcemap: false,
+			format: 'iife',
+			name: 'app',
+			file: `public/build/${page}.js`
+		},
+		plugins: [
+			svelte({
+				preprocess: sveltePreprocess(),
+				compilerOptions: {
+					dev: !production
+				}
+			}),
+			css({ output: `${page}.css` }),
+			resolve({
+				browser: true,
+				dedupe: ['svelte']
+			}),
+			commonjs(),
+			production && terser()
+		]
+	}
+}
+
+const exp = () => {
+	let expPages = [];
+	pages.forEach(page => expPages.push(multiSPA(page)));
+	return expPages;
+}
+
+exp();
+
+export default exp;
---
+++ src/pages/home/Home.svelte
@@ -0,0 +1,9 @@
+<main>
+  <h1>Galavečer 2022</h1>
+</main>
+
+<style>
+  h1 {
+    text-align: center;
+  }
+</style>
