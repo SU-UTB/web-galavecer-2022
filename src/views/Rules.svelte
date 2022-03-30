649fb719a138af38b340a071a4a74aac28761527
tomes_f <filip.tomes3@gmail.com>
2022-03-30 14:37:07 +0200

    styling rules

--- src/views/Rules.svelte
+++ src/views/Rules.svelte
@@ -26,31 +26,33 @@
   const faculties = ['FMK', 'FAME', 'FAI', 'FT', 'FLKŘ', 'FHS']
 </script>
 
-<section id="rules">
-  <h1 class="text-lg uppercase mb-8">Pravidla nominací Galavečer 2022</h1>
-  <ol class="rules ">
-    {#each rulesFour as rule}
-      <li>{rule.txt}</li>
-    {/each}
-    <li>
-      Galavečer UTB má 6 základních kategorií:
-      <ul>
-        {#each faculties as faculty}
-          <li class="text-red uppercase">student {faculty}</li>
-        {/each}
-      </ul>
-      Pozn.: O těchto studentech rozhoduje vedení jednotlivých fakult.
-    </li>
-    <li>
-      Galavečer UTB 2022 má další 2 speciální kategorie:
-      <ul>
-        <li>Sportovec UTB</li>
-        <li>Sportovní tým UTB</li>
-      </ul>
-    </li>
-    <li>Poslední kategorií je <span class="text-red">Studentská cena</span> Studentská cena, do které spadají všichni výše uvedení a o umístění těchto studentů rozhodují zaměstnanci i studenti UTB, ale i široká veřejnost za pomoci svých hlasů (emailových adres) a to pouze jednou z jedné emailové adresy.</li>
-    {#each lastTwoRules as rule}
-      <li>{rule.txt}</li>
-    {/each}
-  </ol>
+<section id="rules" class="py-8">
+  <div class="container px-2 mx-auto">
+    <h1 class="text-3xl uppercase mb-8 text-red text-center">Pravidla nominací<br/>Galavečer 2022</h1>
+    <ol class="rules text-lg">
+      {#each rulesFour as rule}
+        <li class="mb-4 pl-3">{rule.txt}</li>
+      {/each}
+      <li class="mb-4 pl-3">
+        Galavečer UTB má 6 základních kategorií:
+        <ul>
+          {#each faculties as faculty}
+            <li class="text-red uppercase">student {faculty}</li>
+          {/each}
+        </ul>
+        Pozn.: O těchto studentech rozhoduje vedení jednotlivých fakult.
+      </li>
+      <li class="mb-4 pl-3">
+        Galavečer UTB 2022 má další 2 speciální kategorie:
+        <ul>
+          <li class="text-red">Sportovec UTB</li>
+          <li class="text-red">Sportovní tým UTB</li>
+        </ul>
+      </li>
+      <li class="mb-4 pl-3">Poslední kategorií je <span class="text-red">Studentská cena</span> Studentská cena, do které spadají všichni výše uvedení a o umístění těchto studentů rozhodují zaměstnanci i studenti UTB, ale i široká veřejnost za pomoci svých hlasů (emailových adres) a to pouze jednou z jedné emailové adresy.</li>
+      {#each lastTwoRules as rule}
+        <li class="mb-4 pl-3">{rule.txt}</li>
+      {/each}
+    </ol>
+  </div>
 </section>
